1c4df8e687df09c7bf7bb608b3bbcfe75417d4d2
ForAeons <w.xy020203@gmail.com>
2024-08-05 12:57:55 +0800

    day 2 rust solution

---
+++ 2023/02/rust/src/main.rs
@@ -0,0 +1,72 @@
+const RED_KEY: &str = "red";
+const MAX_RED: u32 = 12;
+const GREEN_KEY: &str = "green";
+const MAX_GREEN: u32 = 13;
+const BLUE_KEY: &str = "blue";
+const MAX_BLUE: u32 = 14;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = std::fs::read_to_string("../input.txt")?;
+
+    let sum = input
+        .split("\n")
+        .map(|game| {
+            let temp = game.split(": ").collect::<Vec<_>>();
+            let id = temp[0].split(" ").collect::<Vec<_>>()[1]
+                .parse::<u32>()
+                .unwrap();
+            let sets = temp[1].split("; ").collect::<Vec<_>>();
+
+            let temp = sets
+                .into_iter()
+                .map(|set| {
+                    let pairs = set.split(", ").collect::<Vec<_>>();
+
+                    pairs
+                        .into_iter()
+                        .map(|pair| {
+                            let pair = pair.split(" ").collect::<Vec<_>>();
+                            let count = pair[0];
+                            let color = pair[1];
+                            let count = count.parse::<u32>().unwrap();
+
+                            (color, count)
+                        })
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<Vec<_>>>();
+
+            let mut tabulation = std::collections::HashMap::new();
+            for set in temp {
+                for (color, count) in set {
+                    let current = tabulation.get(color).unwrap_or(&0);
+                    tabulation.insert(color, (*current).max(count));
+                }
+            }
+            (id, tabulation)
+        })
+        .filter(|(_, tabulation)| {
+            let red = tabulation.get(RED_KEY).unwrap_or(&0);
+            if *red > MAX_RED {
+                return false;
+            }
+
+            let green = tabulation.get(GREEN_KEY).unwrap_or(&0);
+            if *green > MAX_GREEN {
+                return false;
+            }
+
+            let blue = tabulation.get(BLUE_KEY).unwrap_or(&0);
+            if *blue > MAX_BLUE {
+                return false;
+            }
+
+            true
+        })
+        .map(|(id, _)| id)
+        .sum::<u32>();
+
+    println!("Sum of IDs: {}", sum);
+
+    Ok(())
+}
