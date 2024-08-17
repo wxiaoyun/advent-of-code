3adb2fe7a860596f6f5ce0cf7fea11b6d488cea1
ForAeons <w.xy020203@gmail.com>
2024-08-17 08:00:42 +0800

    day 14 rust solution refactor

--- 2023/rust/src/day14.rs
+++ 2023/rust/src/day14.rs
@@ -6,66 +6,51 @@ pub fn part_one() -> Result {
         .map(|l| l.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
-    // Stores the row index of the closest '#' above the current cell
-    let mut prefix_sum = mat[0]
-        .iter()
-        .map(|_| vec![-1; mat.len()])
-        .collect::<Vec<_>>();
+    tilt_north(&mut mat);
 
-    for (i, row) in mat.iter().enumerate() {
-        for (j, &c) in row.iter().enumerate() {
-            if c == '#' {
-                prefix_sum[i][j] = i as i64;
+    println!("Part one: {}", calculate_load(&mat));
+
+    Ok(())
+}
+
+fn tilt_north(mat: &mut [Vec<char>]) {
+    fn slide_up(mat: &mut [Vec<char>], i: usize, j: usize) {
+        let mut count = 0;
+        let mut k = i;
+
+        while k < mat.len() && mat[k][j] != '#' {
+            if mat[k][j] == 'O' {
+                count += 1;
             }
+            mat[k][j] = '.';
+            k += 1;
+        }
 
-            if i > 0 {
-                prefix_sum[i][j] = prefix_sum[i][j].max(prefix_sum[i - 1][j]);
+        for l in i..(i + count) {
+            mat[l][j] = 'O';
+        }
+    };
+
+    for i in 0..mat.len() {
+        for j in 0..mat[i].len() {
+            if i == 0 || (i > 0 && mat[i - 1][j] == '#') {
+                slide_up(mat, i, j);
             }
         }
     }
+}
 
-    let rows = mat.len();
-    let cols = mat[0].len();
-
-    for i in 0..rows {
-        for j in 0..cols {
-            let c = mat[i][j];
-            if c != 'O' {
-                continue;
-            }
-
-            let hash_row = prefix_sum[i][j];
-            let new_row = (hash_row + 1) as usize;
-
-            // Swap the current cell with the new cell
-            mat[i][j] = '.';
-            mat[new_row][j] = 'O';
-
-            // Update the prefix sum
-            for k in new_row..mat.len() {
-                if mat[k][j] == '#' {
-                    break;
-                }
-                prefix_sum[k][j] = new_row as i64;
-            }
-        }
-    }
-
-    let res = mat
-        .into_iter()
+fn calculate_load(mat: &[Vec<char>]) -> u64 {
+    mat.iter()
         .enumerate()
         .map(|(r, row)| {
-            row.into_iter().fold(0, |acc, c| {
-                if c == 'O' {
-                    acc + (rows - r) as u64
+            row.iter().fold(0, |acc, c| {
+                if *c == 'O' {
+                    acc + (mat.len() - r) as u64
                 } else {
                     acc
                 }
             })
         })
-        .sum::<u64>();
-
-    println!("Part one: {}", res);
-
-    Ok(())
+        .sum::<u64>()
 }
