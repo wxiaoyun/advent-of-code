80113f193669c1bf5690adffa79d10ad88537093
wxiaoyun <w.xy020203@gmail.com>
2025-12-01 22:27:10 +0800

    chore: rename rust module

--- 2025/rust/src/main.rs
+++ 2025/rust/src/main.rs
@@ -1,11 +1,11 @@
-mod d01;
+mod day01;
 
 fn main() {
     let args = util::parse_args();
 
     match (args.day, args.part) {
-        (1, 1) => d01::part_one(),
-        (1, 2) => d01::part_two(),
+        (1, 1) => day01::part_one(),
+        (1, 2) => day01::part_two(),
         _ => panic!("Day {}, part {} not implemented", args.day, args.part),
     }
 }
