4edf1e80a2fa324515855e4c4eedc20b697b1963
wxiaoyun <w.xy020203@gmail.com>
2025-12-02 16:18:12 +0800

    rust refactor

--- 2025/rust/src/day02.rs
+++ 2025/rust/src/day02.rs
@@ -11,64 +11,67 @@ fn digits(self: i64) -> Vec<u8> {
     digits
 }
 
-pub fn part_one(input: impl AsRef<str>) -> i64 {
-    let ranges = input.as_ref().split(',').map(|r| {
-        let mut range = r.split('-');
-        let mut extract_num = || {
-            range
-                .next()
-                .and_then(|s| s.trim().parse::<i64>().ok())
-                .unwrap()
+struct InvalidFinder {
+    left: i64,
+    bound: i64,
+}
+
+impl InvalidFinder {
+    fn new(l: i64, r: i64) -> Self {
+        let l_digits = l.digits().len() as u32;
+        let left = if l_digits.is_multiple_of(2) {
+            let modd = 10i64.pow(l_digits / 2);
+            let l_num = l / modd;
+            let r_num = l % modd;
+
+            if l_num < r_num { l_num + 1 } else { l_num }
+        } else {
+            let half_len = l_digits / 2;
+            10i64.pow(half_len)
         };
-        (extract_num(), extract_num())
-    });
 
-    struct InvalidFinder {
-        left: i64,
-        bound: i64,
+        InvalidFinder { left, bound: r }
     }
+}
 
-    impl InvalidFinder {
-        fn new(l: i64, r: i64) -> Self {
-            let l_digits = l.digits().len() as u32;
-            let left = if l_digits.is_multiple_of(2) {
-                let modd = 10i64.pow(l_digits / 2);
-                let l_num = l / modd;
-                let r_num = l % modd;
+impl Iterator for InvalidFinder {
+    type Item = i64;
 
-                if l_num < r_num { l_num + 1 } else { l_num }
-            } else {
-                let half_len = l_digits / 2;
-                10i64.pow(half_len)
+    fn next(&mut self) -> Option<Self::Item> {
+        let left_digits = self.left.digits();
+        let next_invalid = left_digits
+            .iter()
+            .rev()
+            .cycle()
+            .take(left_digits.len() * 2)
+            .copied()
+            .fold(0, |acc, n| acc * 10 + n as i64);
+
+        if next_invalid > self.bound {
+            return None;
+        }
+
+        self.left += 1;
+        Some(next_invalid)
+    }
+}
+
+pub fn part_one(input: impl AsRef<str>) -> i64 {
+    input
+        .as_ref()
+        .split(',')
+        .map(|r| {
+            let mut range = r.split('-');
+            let mut extract_num = || {
+                range
+                    .next()
+                    .and_then(|s| s.trim().parse::<i64>().ok())
+                    .unwrap()
             };
-
-            InvalidFinder { left, bound: r }
-        }
-    }
-
-    impl Iterator for InvalidFinder {
-        type Item = i64;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            let left_digits = self.left.digits();
-            let next_invalid = left_digits
-                .iter()
-                .rev()
-                .cycle()
-                .take(left_digits.len() * 2)
-                .copied()
-                .fold(0, |acc, n| acc * 10 + n as i64);
-
-            if next_invalid > self.bound {
-                return None;
-            }
-
-            self.left += 1;
-            Some(next_invalid)
-        }
-    }
-
-    ranges.flat_map(|(l, r)| InvalidFinder::new(l, r)).sum()
+            (extract_num(), extract_num())
+        })
+        .flat_map(|(l, r)| InvalidFinder::new(l, r))
+        .sum()
 }
 
 pub fn part_two(input: impl AsRef<str>) -> i64 {
