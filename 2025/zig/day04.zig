f8438d65d7a407cd9193bdefb47c42dbf36c9397
wxiaoyun <w.xy020203@gmail.com>
2025-12-05 09:33:36 +0800

    zig 2025 day 4

--- 2025/zig/day04.zig
+++ 2025/zig/day04.zig
@@ -7,16 +7,13 @@ pub fn main() !void {
     const alloc = gpa.allocator();
     defer _ = gpa.deinit();
 
-    const arena_alloc = std.heap.ArenaAllocator.init(alloc);
-    defer arena_alloc.deinit();
-    const aalloc = arena_alloc.allocator();
-
     const args = try util.parseArgs();
-    const input = try util.readInputFromStdin(aalloc);
+    const input = try util.readInputFromStdin(alloc);
+    defer alloc.free(input);
 
     const result = switch (args.part) {
-        1 => try part1(aalloc, input),
-        2 => try part2(aalloc, input),
+        1 => try part1(alloc, input),
+        2 => try part2(alloc, input),
         else => @panic("Illegal part"),
     };
 
@@ -29,7 +26,7 @@ fn part1(_: std.mem.Allocator, input: []u8) !i64 {
     var ncol: usize = 0;
     while (rows.next()) |row| : (nrow += 1) {
         if (row.len == 0) {
-            continue;
+            break;
         }
         ncol = row.len;
     }
@@ -44,12 +41,6 @@ fn part1(_: std.mem.Allocator, input: []u8) !i64 {
     }
 
     return n_accessible;
-}
-
-fn part2(alloc: std.mem.Allocator, input: []u8) !i64 {
-    _ = alloc;
-    _ = input;
-    return 0;
 }
 
 fn isAccessible(mat: []u8, nrow: usize, ncol: usize, r: usize, c: usize) bool {
@@ -89,6 +80,75 @@ fn isAccessible(mat: []u8, nrow: usize, ncol: usize, r: usize, c: usize) bool {
     return roll_cnt < 4;
 }
 
+fn part2(alloc: std.mem.Allocator, input: []u8) !i64 {
+    var rows = std.mem.splitScalar(u8, input, '\n');
+    var nrow: usize = 0;
+    var ncol: usize = 0;
+    while (rows.next()) |row| : (nrow += 1) {
+        if (row.len == 0) {
+            break;
+        }
+        ncol = row.len;
+    }
+
+    // Holds pairs of (row, col) tuples
+    var work_queue = std.ArrayList(usize).empty;
+    defer work_queue.deinit(alloc);
+    try work_queue.ensureTotalCapacity(alloc, nrow * ncol * 2);
+
+    for (0..nrow) |i| {
+        for (0..ncol) |j| {
+            if (isAccessible(input, nrow, ncol, i, j)) {
+                try work_queue.append(alloc, i);
+                try work_queue.append(alloc, j);
+            }
+        }
+    }
+
+    var n_accessible: i64 = 0;
+    while (work_queue.items.len > 1) {
+        const c = work_queue.pop().?;
+        const r = work_queue.pop().?;
+        if (isAccessible(input, nrow, ncol, r, c)) {
+            try removeSelfAndNotifyNeighbor(alloc, &work_queue, input, nrow, ncol, r, c);
+            n_accessible += 1;
+        }
+    }
+    return n_accessible;
+}
+
+fn removeSelfAndNotifyNeighbor(alloc: std.mem.Allocator, work_queue: *std.ArrayList(usize), mat: []u8, nrow: usize, ncol: usize, r: usize, c: usize) !void {
+    const row_len = ncol + 1;
+
+    mat[r * row_len + c] = '.';
+
+    var r_start: usize = 0;
+    if (r > 0) {
+        r_start = r - 1;
+    }
+    var r_end = nrow;
+    if (r + 2 <= nrow) {
+        r_end = r + 2;
+    }
+    var c_start: usize = 0;
+    if (c > 0) {
+        c_start = c - 1;
+    }
+    var c_end = ncol;
+    if (c + 2 <= ncol) {
+        c_end = c + 2;
+    }
+
+    for (r_start..r_end) |rr| {
+        for (c_start..c_end) |cc| {
+            if (mat[rr * row_len + cc] == '@') {
+                try work_queue.append(alloc, rr);
+                try work_queue.append(alloc, cc);
+            }
+        }
+    }
+}
+
 const test_input =
     \\..@@.@@@@.
     \\@@@.@.@.@@
@@ -115,5 +175,5 @@ test part2 {
     const input = try alc.alloc(u8, test_input.len);
     defer alc.free(input);
     @memcpy(input, test_input);
-    try std.testing.expectEqual(43, try part2(alc, test_input));
+    try std.testing.expectEqual(43, try part2(alc, input));
 }
