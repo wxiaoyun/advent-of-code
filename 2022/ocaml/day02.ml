50d52664da4bc226eacfcb8f20b440f595d06c7c
wxiaoyun <w.xy020203@gmail.com>
2025-12-10 15:22:48 +0800

    2022 ocaml day 2 part 2

--- 2022/ocaml/day02.ml
+++ 2022/ocaml/day02.ml
@@ -1,41 +1,83 @@
 type rps = Rock | Paper | Scissors
 
-let rps_from_str = function
+let mov_from_str = function
   | "A" | "X" -> Rock
   | "B" | "Y" -> Paper
   | "C" | "Z" -> Scissors
   | _ -> failwith "bad rps str"
 
+let mov_score = function Rock -> 1 | Paper -> 2 | Scissors -> 3
+
+type outcome = Win | Draw | Loss
+
+let outcome_from_str = function
+  | "X" -> Loss
+  | "Y" -> Draw
+  | "Z" -> Win
+  | _ -> failwith "bad rps str"
+
+let outcome_score = function Win -> 6 | Draw -> 3 | Loss -> 0
+
 let get_score opp_mov self_mov =
-  let mov_score =
-    match self_mov with Rock -> 1 | Paper -> 2 | Scissors -> 3
-  in
-
-  let win = 6 in
-  let draw = 3 in
-  let loss = 0 in
-  let outcome_score =
+  let outcome =
     match (opp_mov, self_mov) with
-    | a, b when a = b -> draw
-    | Rock, Paper -> win
-    | Rock, _ -> loss
-    | Paper, Scissors -> win
-    | Paper, _ -> loss
-    | Scissors, Rock -> win
-    | Scissors, _ -> loss
+    | a, b when a = b -> Draw
+    | Rock, Paper -> Win
+    | Rock, _ -> Loss
+    | Paper, Scissors -> Win
+    | Paper, _ -> Loss
+    | Scissors, Rock -> Win
+    | Scissors, _ -> Loss
   in
 
-  mov_score + outcome_score
+  mov_score self_mov + outcome_score outcome
 
 let part_one input =
   input |> String.split_on_char '\n'
   |> List.map (fun line ->
-      line |> String.split_on_char ' ' |> List.map rps_from_str |> function
+      line |> String.split_on_char ' ' |> List.map mov_from_str |> function
       | [ opp; self ] -> get_score opp self
       | _ -> failwith "bad input")
   |> List.fold_left Int.add 0
 
-let part_two _ = failwith "unimplemented"
+let get_score_from_strategy opp_mov outcome =
+  let movs = [ Rock; Paper; Scissors ] in
+
+  let get_winning_mov_against mov =
+    let i =
+      match List.find_index (fun m -> m = mov) movs with
+      | Some i -> i
+      | None -> failwith "unreachable"
+    in
+    List.nth movs (Int.rem (i + 1) 3)
+  in
+
+  let get_losing_mov_against mov =
+    let i =
+      match List.find_index (fun m -> m = mov) movs with
+      | Some i -> i
+      | None -> failwith "unreachable"
+    in
+    List.nth movs (Int.rem (3 + i - 1) 3)
+  in
+
+  let self_mov =
+    match outcome with
+    | Draw -> opp_mov
+    | Win -> get_winning_mov_against opp_mov
+    | Loss -> get_losing_mov_against opp_mov
+  in
+
+  mov_score self_mov + outcome_score outcome
+
+let part_two input =
+  input |> String.split_on_char '\n'
+  |> List.map (fun line ->
+      line |> String.split_on_char ' ' |> function
+      | [ opp; outcome ] ->
+          get_score_from_strategy (mov_from_str opp) (outcome_from_str outcome)
+      | _ -> failwith "bad input")
+  |> List.fold_left Int.add 0
 
 let () =
   let part = Aoc.Util.parse_args () in
