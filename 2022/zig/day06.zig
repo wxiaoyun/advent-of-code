9ad3910632749946bb4d98120b44eae7090d0046
wxiaoyun <w.xy020203@gmail.com>
2025-12-22 14:38:05 +0800

    zig 2022 day 6 part 2

--- 2022/zig/day06.zig
+++ 2022/zig/day06.zig
@@ -55,6 +55,37 @@ fn part1(alloc: std.mem.Allocator, input: []u8) !i64 {
     return -1;
 }
 
-fn part2(_: std.mem.Allocator, _: []u8) !i64 {
-    return 0;
+fn part2(alloc: std.mem.Allocator, input: []u8) !i64 {
+    const n_distinct = 14;
+
+    var cs = CharMap.init(alloc);
+    defer cs.deinit();
+    try cs.ensureTotalCapacity(n_distinct + 1);
+
+    for (input, 0..) |c, i| {
+        {
+            const entry = cs.getOrPutAssumeCapacity(c);
+            if (entry.found_existing) {
+                entry.value_ptr.* += 1;
+            } else {
+                entry.value_ptr.* = 1;
+            }
+        }
+
+        if (i >= n_distinct) {
+            const key = input[i - n_distinct];
+            const entry = cs.getEntry(key).?;
+            entry.value_ptr.* -= 1;
+            if (entry.value_ptr.* == 0) {
+                _ = cs.remove(key);
+            }
+        }
+
+        if (cs.unmanaged.size == n_distinct) {
+            const idx: i64 = @intCast(i);
+            return idx + 1;
+        }
+    }
+
+    return -1;
 }
