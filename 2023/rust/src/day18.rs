2ab5f2ce08d76d12b0189c194c5873aa772217b8
wxiaoyun <w.xy020203@gmail.com>
2026-06-06 17:19:17 +0900

    rust 2023 day 18 part 2

--- 2023/rust/src/day18.rs
+++ 2023/rust/src/day18.rs
@@ -23,6 +23,7 @@ pub fn part_one(input: impl AsRef<str>) -> i64 {
 
     // println!("{:?}", vertices);
 
+    // Shoelace formula to calculate area
     let area = (0..vertices.len() as i64)
         .map(|i| {
             (vertices[i as usize].0
@@ -34,11 +35,63 @@ pub fn part_one(input: impl AsRef<str>) -> i64 {
         .abs()
         / 2;
 
+    // Pick's theorem to calculate size
     area + boundary_len / 2 + 1
 }
 
 pub fn part_two(input: impl AsRef<str>) -> i64 {
-    0
+    let plan = parse_input(input).into_iter().map(|(_, _, s)| {
+        let chars = s.chars().skip(2).take(6).collect::<Vec<_>>();
+        let dir = match chars.last().unwrap() {
+            '0' => 'R',
+            '1' => 'D',
+            '2' => 'L',
+            '3' => 'U',
+            c => unreachable!(),
+        };
+
+        let step = chars
+            .iter()
+            .take(5)
+            .fold(0, |acc, c| acc * 16 + c.to_digit(16).unwrap()) as i64;
+
+        (dir, step, ' ')
+    });
+
+    let mut vertices = Vec::new();
+    let (mut r, mut c) = (0, 0);
+    let mut boundary_len = 0;
+    for (dir, step, _) in plan {
+        let (dr, dc) = match dir {
+            'U' => (-1, 0),
+            'D' => (1, 0),
+            'R' => (0, 1),
+            'L' => (0, -1),
+            _ => unreachable!(),
+        };
+
+        r = r + dr * step;
+        c = c + dc * step;
+        vertices.push((r, c));
+        boundary_len += step;
+    }
+
+    // println!("{:?}", vertices);
+
+    // Shoelace formula to calculate area
+    let area = (0..vertices.len() as i64)
+        .map(|i| {
+            (vertices[i as usize].0
+                * vertices[(i + 1).rem_euclid(vertices.len() as i64) as usize].1)
+                - (vertices[i as usize].0
+                    * vertices[(i - 1).rem_euclid(vertices.len() as i64) as usize].1)
+        })
+        .sum::<i64>()
+        .abs()
+        / 2;
+
+    // Pick's theorem to calculate size
+    area + boundary_len / 2 + 1
 }
 
 fn parse_input(input: impl AsRef<str>) -> Vec<(char, i64, String)> {
