51c9a2939816a8872774caccb6c9aa4defda359f
wxiaoyun <w.xy020203@gmail.com>
2026-06-17 18:47:35 +0800

    rust 2023 day 23 part 2 (bruteforce)

--- 2023/rust/src/day23.rs
+++ 2023/rust/src/day23.rs
@@ -1,31 +1,12 @@
 use std::collections::HashSet;
 
+const PATH: char = '.';
+const FOREST: char = '#';
+
 pub fn part_one(input: impl AsRef<str>) -> i64 {
     let grid = parse_input(input);
-
-    dfs(&grid)
-}
-
-pub fn part_two(input: impl AsRef<str>) -> i64 {
-    let _ = parse_input(input);
-
-    unimplemented!()
-}
-
-fn parse_input(input: impl AsRef<str>) -> Vec<Vec<char>> {
-    input
-        .as_ref()
-        .lines()
-        .map(|l| l.chars().collect())
-        .collect()
-}
-
-fn dfs(grid: &Vec<Vec<char>>) -> i64 {
     let (m, n) = (grid.len(), grid[0].len());
     let mut max_steps = 0;
-
-    const PATH: char = '.';
-    const FOREST: char = '#';
 
     let mut stack = vec![(0, 0, 1, 0, 0)];
     while let Some(a) = stack.pop() {
@@ -47,7 +28,7 @@ fn dfs(grid: &Vec<Vec<char>>) -> i64 {
                         .for_each(|coord| nexts.push(coord))
                 }
             }
-            FOREST => {}
+            FOREST => continue,
             '>' => nexts.push((r, c + 1)),
             '<' => nexts.push((r, c - 1)),
             'v' => nexts.push((r + 1, c)),
@@ -63,4 +44,56 @@ fn dfs(grid: &Vec<Vec<char>>) -> i64 {
     }
 
     max_steps
+}
+
+pub fn part_two(input: impl AsRef<str>) -> i64 {
+    let grid = parse_input(input);
+
+    fn dfs(
+        grid: &Vec<Vec<char>>,
+        visited: &mut Vec<Vec<bool>>,
+        r: usize,
+        c: usize,
+        dist: i64,
+    ) -> i64 {
+        let (m, n) = (grid.len(), grid[0].len());
+        if (r, c) == (m - 1, n - 2) {
+            return dist;
+        }
+
+        visited[r][c] = true;
+
+        let res = match grid[r][c] {
+            FOREST => 0,
+            _ => {
+                if (r, c) == (0, 1) {
+                    dfs(grid, visited, r + 1, c, dist + 1)
+                } else {
+                    let mut max_dist = 0;
+                    for (rr, cc) in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)] {
+                        if visited[rr][cc] || grid[rr][cc] == FOREST {
+                            continue;
+                        }
+                        max_dist = max_dist.max(dfs(grid, visited, rr, cc, dist + 1));
+                    }
+                    max_dist
+                }
+            }
+        };
+
+        visited[r][c] = false;
+        res
+    }
+
+    let (m, n) = (grid.len(), grid[0].len());
+    let mut visited = vec![vec![false; n]; m];
+    dfs(&grid, &mut visited, 0, 1, 0)
+}
+
+fn parse_input(input: impl AsRef<str>) -> Vec<Vec<char>> {
+    input
+        .as_ref()
+        .lines()
+        .map(|l| l.chars().collect())
+        .collect()
 }
